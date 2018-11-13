c3bdc8ba20eb3d18932ade5aa95b9e5792608fd8
sk784 <sk784@yandex.ru>
2018-11-13 14:47:20 +0300

    задание 7

---
+++ zadanie7/src/Server/MainServer.java
@@ -0,0 +1,86 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.*;
+
+public class MainServer {
+
+    Vector<ClientHandler> clients;
+
+    public MainServer() throws SQLException {
+
+        ServerSocket server = null;
+        Socket socket = null;
+        clients = new Vector<>();
+
+        try {
+            AuthService.connect();
+
+            server = new ServerSocket(8180);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился!");
+                // создаем нового клиента
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+
+    }
+    // метод для рассылки сообщения всем клиентам
+    public void broadCastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    // метод для рассылки сообщения конкретному клиенту
+    public void privateMsg(String nick,String msg) {
+        Map<String, ClientHandler> hm = new HashMap<>();
+        Set<Map.Entry<String, ClientHandler>> set = hm.entrySet();
+          for(Map.Entry<String,ClientHandler> me: set) {
+            if(me.getKey().equalsIgnoreCase(nick))
+              me.getValue().sendMsg(msg);
+            else
+                System.out.println("В сети нет такого пользователя!");
+        }
+    }
+
+
+
+    // подписываем клиента и добавляем его в список клиентов
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    // отписываем клиента и удаляем его из списка клиентов
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+}
